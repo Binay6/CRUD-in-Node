b1a8021a74abeceae66609e20aae3fc9b23d7ff0
Binay6 <binay3662@gmail.com>
2024-03-20 11:12:21 +0530

    a message neede to stage change

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express() ;
 app.use(bodyParser.urlencoded({extended:true})) ;
 app.use(express.json()) ;
 
-mongoose.connect('url',{
+mongoose.connect('mongodb+srv://binay3662:8rSsKVVCooxJWxN7@cluster0.dlhk7wc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
